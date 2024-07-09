cf4b1351f5f8f5a2b73169934f6f339568f3ddd7
Youssef Saaid <youssefadel@apessmartsolutions.com>
2024-07-09 21:14:49 +0300

    comment some vars i don't use

--- nvim/.config/nvim/lua/after/luasnip.lua
+++ nvim/.config/nvim/lua/after/luasnip.lua
@@ -4,11 +4,11 @@ local t = ls.text_node
 local i = ls.insert_node
 local extras = require("luasnip.extras")
 local rep = extras.rep
-local fmt = require("luasnip.extras.fmt").fmt
-local c = ls.choice_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local sn = ls.snippet_node
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local c = ls.choice_node
+-- local f = ls.function_node
+-- local d = ls.dynamic_node
+-- local sn = ls.snippet_node
 
 vim.keymap.set({ "i", "s" }, "<A-n>", function()
 	if ls.choice_active() then
