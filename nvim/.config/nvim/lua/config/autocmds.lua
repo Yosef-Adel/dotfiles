cea902a1ce977dcea058f1add02d48c716b39400
Youssef Saaid <youssefadel@apessmartsolutions.com>
2024-07-31 22:54:20 +0300

    adding a warning when entirng a nodemodules or when trying to write on nodemodule

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,17 +6,28 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- Create the autocmd
+-- To Warn you when writing in node_modules
 vim.api.nvim_create_autocmd("BufWritePre", {
 	desc = "Notify if the buffer is within node_modules",
 	callback = function()
-		-- Get the current buffer's file path
 		local notify = require("notify")
 		local buf_path = vim.fn.expand("%:p")
-		-- Check if the path contains 'node_modules'
 		if string.find(buf_path, "node_modules") then
-			-- Trigger the notification
-			notify("Warning: You are writing a file in node_modules!", "warn", {
+			notify("Warning: You are writing a file in node_modules!", "error", {
+				title = "Node Modules Warning",
+			})
+		end
+	end,
+})
+
+-- To warn you when entering node_modules file
+vim.api.nvim_create_autocmd("BufEnter", {
+	desc = "Notify if the buffer is within node_modules",
+	callback = function()
+		local notify = require("notify")
+		local buf_path = vim.fn.expand("%:p")
+		if string.find(buf_path, "node_modules") then
+			notify("Warning: You entered a file in node_modules!", "warn", {
 				title = "Node Modules Warning",
 			})
 		end
