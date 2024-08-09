bdc3043355a2a0790d4c5cb5c001a67c36167b3d
Yosef Adel <yosef.saaid02@eng-st.cu.edu.eg>
2024-08-09 21:09:48 +0300

    adding color when yank

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,10 +1,14 @@
 local group = vim.api.nvim_create_augroup("NeoJoey_Event_Group", { clear = true })
 
--- for highlight on yank
+vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#6EACDA", fg = "#021526" })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	callback = function()
-		vim.highlight.on_yank()
+		vim.highlight.on_yank({
+			higroup = "YankHighlight",
+			timeout = 200,
+		})
 	end,
 	group = group,
 })
