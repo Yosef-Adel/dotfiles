af89676561617bb7cdec368b493ac9aac24743ef
Yosef-Adel <yosefadel002@gmail.com>
2024-11-09 18:51:42 +0200

    edit remaps

--- wezterm/.config/wezterm/utils/keys.lua
+++ wezterm/.config/wezterm/utils/keys.lua
@@ -10,7 +10,7 @@ M.key_table = function(mods, key, action)
 end
 
 M.cmd_key = function(key, action)
-	return M.key_table("WIN", key, action)
+	return M.key_table("ALT", key, action)
 end
 
 M.cmd_to_tmux_prefix = function(key, tmux_key)
