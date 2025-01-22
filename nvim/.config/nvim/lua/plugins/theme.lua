4304485f896d309c4609956e42f279dbfdb8bc09
Yosef-Adel <yosefadel002@gmail.com>
2025-01-22 08:58:55 +0200

    edit the theme to tokyo night and the bar in the bottom

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -25,6 +25,67 @@ return {
 				lualine_bold = false,
 			})
 			vim.cmd([[colorscheme tokyonight]])
+
+			-- Ensure the status line is visible
+			vim.opt.laststatus = 2
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			local colors = {
+				blue = "#7aa2f7",
+				cyan = "#7dcfff",
+				black = "#1a1b26",
+				white = "#c0caf5",
+				red = "#f7768e",
+				violet = "#bb9af7",
+				grey = "#24283b",
+			}
+			local bubbles_theme = {
+				normal = {
+					a = { fg = colors.black, bg = colors.violet },
+					b = { fg = colors.white, bg = colors.grey },
+					c = { fg = colors.white },
+				},
+				insert = { a = { fg = colors.black, bg = colors.blue } },
+				visual = { a = { fg = colors.black, bg = colors.cyan } },
+				replace = { a = { fg = colors.black, bg = colors.red } },
+				inactive = {
+					a = { fg = colors.white, bg = colors.black },
+					b = { fg = colors.white, bg = colors.black },
+					c = { fg = colors.white },
+				},
+			}
+			require("lualine").setup({
+				options = {
+					theme = bubbles_theme,
+					component_separators = "",
+					section_separators = { left = "", right = "" },
+					globalstatus = true,
+				},
+				sections = {
+					lualine_a = { { "mode", separator = { left = "" }, right_padding = -4 } },
+					lualine_b = { "filename", "branch", "diff" },
+					lualine_c = {},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = {
+						{ "location", separator = { right = "" }, left_padding = -4 },
+					},
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				tabline = {},
+				extensions = {},
+			})
 		end,
 	},
 }
