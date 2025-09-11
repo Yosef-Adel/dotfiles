80517c04d9d30f7529e208077044c5f79423584d
Yosef-Adel <yosefadel002@gmail.com>
2025-09-11 17:02:28 +0300

    add nvim-tree and refactor harpon

---
+++ nvim/.config/nvim/lua/plugins/file-tree.lua
@@ -0,0 +1,92 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-p>"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
+
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "<Leader>e", ":Oil<CR>", { silent = true, noremap = true })
+		end,
+	},
+
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" }, -- ensure devicons
+		cmd = { "NvimTreeToggle", "NvimTreeFindFile" },
+		keys = {
+			{ "<leader>t", "<cmd>NvimTreeToggle<cr>", desc = "Toggle file tree" },
+			{ "<leader>T", "<cmd>NvimTreeFindFile<cr>", desc = "Reveal in file tree" },
+		},
+		opts = {
+			-- Oil is the directory hijacker
+			disable_netrw = true,
+			hijack_netrw = false,
+			hijack_directories = { enable = false },
+
+			sync_root_with_cwd = true,
+			update_focused_file = { enable = true, update_root = false },
+
+			diagnostics = {
+				enable = true,
+				show_on_dirs = true,
+				icons = { hint = "", info = "", warning = "", error = "" },
+			},
+
+			view = { width = 36 },
+			renderer = {
+				root_folder_label = false,
+				highlight_git = true,
+				indent_markers = { enable = true },
+
+				icons = {
+					webdev_colors = true, -- use devicons colors
+					git_placement = "before",
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "",
+						symlink = "",
+						bookmark = "",
+						folder = {
+							arrow_closed = "",
+							arrow_open = "",
+							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							symlink = "",
+							symlink_open = "",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+			},
+
+			filters = { dotfiles = false },
+			git = { enable = true, ignore = false },
+		},
+	},
+}
--- nvim/.config/nvim/lua/plugins/harpon.lua
+++ nvim/.config/nvim/lua/plugins/harpon.lua
@@ -1,5 +1,6 @@
 return {
 	"theprimeagen/harpoon",
+	branch = "harpoon2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
@@ -7,36 +8,14 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add file to harpoon" })
-		vim.keymap.set("n", "<leader>t", ui.toggle_quick_menu, { desc = "Toggle harpoon menu" })
+		vim.keymap.set("n", "ma", mark.add_file, { desc = "Add file to harpoon" })
+		vim.keymap.set("n", "mm", ui.toggle_quick_menu, { desc = "Toggle harpoon menu" })
 
-		-- numbers
-		vim.keymap.set("n", "<leader>1", function()
-			ui.nav_file(1)
-		end, { desc = "Navigate to 1st file" })
-		vim.keymap.set("n", "<leader>2", function()
-			ui.nav_file(2)
-		end, { desc = "Navigate to 2nd file" })
-		vim.keymap.set("n", "<leader>3", function()
-			ui.nav_file(3)
-		end, { desc = "Navigate to 3rd file" })
-		vim.keymap.set("n", "<leader>4", function()
-			ui.nav_file(4)
-		end, { desc = "Navigate to 4th file" })
-		vim.keymap.set("n", "<leader>5", function()
-			ui.nav_file(5)
-		end, { desc = "Navigate to 5th file" })
-		vim.keymap.set("n", "<leader>6", function()
-			ui.nav_file(6)
-		end, { desc = "Navigate to 6th file" })
-		vim.keymap.set("n", "<leader>7", function()
-			ui.nav_file(7)
-		end, { desc = "Navigate to 7th file" })
-		vim.keymap.set("n", "<leader>8", function()
-			ui.nav_file(8)
-		end, { desc = "Navigate to 8th file" })
-		vim.keymap.set("n", "<leader>9", function()
-			ui.nav_file(9)
-		end, { desc = "Navigate to 9th file" })
+		-- Navigate to files by index (1-9)
+		for i = 1, 9 do
+			vim.keymap.set("n", "m" .. i, function()
+				ui.nav_file(i)
+			end, { desc = "Navigate to " .. i .. " file" })
+		end
 	end,
 }
