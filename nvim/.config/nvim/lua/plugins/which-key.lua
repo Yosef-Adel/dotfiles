a46aef2cf3c45601719e4e9451ef273ce6bcb079
Yosef-Adel <yosefadel002@gmail.com>
2026-01-23 10:13:02 +0200

    feat: remove redunduct keymaps, add vim as a globals var

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,7 +1,21 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
 	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+	event = "VimEnter",
+	config = function()
+		local wk = require("which-key")
+		wk.setup()
+
+		wk.add({
+			{ "<leader>s", group = "Search" },
+			{ "<leader>r", group = "Refactor" },
+			{ "<leader>x", group = "Trouble" },
+			{ "<leader>c", group = "Code" },
+			{ "<leader>p", group = "Project" },
+			{ "<leader>v", group = "View" },
+			{ "<leader>f", group = "Find/Float" },
+			{ "<leader>n", group = "Notes/Tabs" },
+			{ "<leader>t", group = "Tree" },
+			{ "m", group = "Marks/Harpoon" },
+		})
 	end,
 }
