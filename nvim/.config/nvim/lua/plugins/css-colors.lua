b8b25bea81ce31cc02c5550e5dcbe844b909f37e
Yosef-Adel <yosefadel002@gmail.com>
2026-04-21 19:19:13 +0200

    refactor: replace colorizer, switch to kulala, remove vim-signature

--- nvim/.config/nvim/lua/plugins/css-colors.lua
+++ nvim/.config/nvim/lua/plugins/css-colors.lua
@@ -1,7 +1,5 @@
 return {
-	"norcalli/nvim-colorizer.lua",
-	opt = {},
-	config = function()
-		require("colorizer").setup()
-	end,
+	"NvChad/nvim-colorizer.lua",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {},
 }
